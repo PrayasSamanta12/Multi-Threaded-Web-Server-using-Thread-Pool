66e0263aa4dd77cc72b45943b18b7c5b46d3dbfa
PrayasSamanta12 <ps19july2003@gmail.com>
2024-08-29 20:40:59 +0530

    Initial commit

--- MultiThreaded/Server.java
+++ MultiThreaded/Server.java
@@ -34,11 +34,21 @@ public class Server {
             serverSocket.setSoTimeout(70000);
             System.out.println("Server is listening on port " + port);
             while (true) {
+                //Creating a new socket for communication whenever request is received
                 Socket clientSocket = serverSocket.accept();
 
                 // Create and start a new thread for each client
+                //We are overriding the run method using lambda expression[Approach 1]
                 Thread thread = new Thread(() -> server.getConsumer().accept(clientSocket));
+                //[Approch 2]-> Using Annonymous CLass
+                // Thread thread2 = new Thread(new Runnable(){
+                //     @Override
+                //     public void run() {
+                //         server.getConsumer().accept(clientSocket);
+                //     }
+                // });
                 thread.start();
+                //We are not closing the connection because if connection is closed then it will not accept any further requests
             }
         } catch (IOException ex) {
             ex.printStackTrace();
